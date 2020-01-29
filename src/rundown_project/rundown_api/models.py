49f00f6ae38c24402efb09379c34b4786e4a7c8a
ydhnwb <yudhanewbie@gmail.com>
2020-01-30 00:14:54 +0700

    create a models

--- src/rundown_project/rundown_api/models.py
+++ src/rundown_project/rundown_api/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager,AbstractBaseUser,PermissionsMixin
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    def create_user(self, email, name, password = None):
+        if not email:
+            raise ValueError("User must have an email address")
+        email = self.normalize_email(email)
+        user = self.model(email = email, name = name)
+        user.set_password(password)
+        user.save(using = self._db)
+        return user
+
+    def create_superuser(self, email, name, password):
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using = self._db)
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.email
+
+
+class Rundown(models.Model):
+    user_profile = models.ForeignKey(UserProfile, on_delete = models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_on = models.DateTimeField(auto_now=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    is_trashed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+class RundownDetail(models.Model):
+    rundown = models.ForeignKey(Rundown, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    order_num = models.IntegerField(default=0)
+    with_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Friend(models.Model):
+    user_profile = models.ForeignKey(UserProfile, on_delete= models.CASCADE)
+    friend_id = models.ForeignKey(UserProfile, on_delete= models.CASCADE)
+    is_blocked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.user_profile)
--- src/rundown_project/rundown_project/settings.py
+++ src/rundown_project/rundown_project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rundown_api'
+
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'rundown_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rundown',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
